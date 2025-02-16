9b2a161b4d5fee28cd1d348bacef9524d2e1470d
Lais Ponte Costa <laisdaguerra@gmail.com>
2025-02-16 13:35:31 -0300

    finalizado

--- script.js
+++ script.js
@@ -12,31 +12,30 @@ function verificar() {
         var img = document.createElement ('img')
         img.setAttribute ('id', 'foto')
         if(sex[0].checked){
-            gen = 'Homem'
+            gen = 'um homem'
             if(idade >= 0 && idade < 10 ){
-                //criança
-                img.setAttribute ('scr','meninokd.png')
+                img.setAttribute ('src','meninokd.png')
             }else if (idade < 21) {
-                // jovem 
-                img.setAttribute ('scr','meninokd.png')
+                img.setAttribute ('src','menino.png')
             }else if (idade < 50 ) {
-                // adulto
+                img.setAttribute ('src','adulto.png')
             }else{
-                // idoso
+                img.setAttribute ('src','idoso.png')
             }
         } else if (sex[1].checked){
-            gen =  'Mulher'
+            gen =  'uma mulher'
             if(idade >= 0 && idade < 10 ){
-                //criança
+                img.setAttribute ('src','meninakd.png')
             }else if (idade < 21) {
-                // jovem 
+                img.setAttribute ('src','menina.png')
             }else if (idade < 50 ) {
-                // adulto
+                img.setAttribute ('src','adulta.png')
             }else{
-                // idoso
+                img.setAttribute ('src','idosa.png')
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gen} com ${idade}`
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
